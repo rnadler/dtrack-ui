6e34666e5476f6321c9a4143a7a575d284e6bf0c
rnadler <robert.nadler@gmail.com>
2017-01-14 21:19:58 -0800

    T-42: Update npm dependencies and fix ng2-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
+import { AlertModule } from 'ng2-bootstrap';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChartModule } from 'angular2-highcharts';
 
@@ -67,7 +67,7 @@ type StoreType = {
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule,
+    AlertModule.forRoot(),
     ChartModule,
     NgbModule.forRoot(),
     RouterModule.forRoot(ROUTES, { useHash: true })
